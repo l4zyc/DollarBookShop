73597af898eaee1645efb31e71247c55cc0c1426
l4zyC <appleyoukick@gmail.com>
2024-10-21 23:51:39 +0700

    Commit Init

--- DollarBookShop/src/controller/LoginController.java
+++ DollarBookShop/src/controller/LoginController.java
@@ -8,8 +8,30 @@ public class LoginController {
 		
 	}
 	
-	public void validateEmail(String email) {
-	
+	public boolean validateEmail(String email) {
+		Integer atIndex = email.indexOf('@');
+		
+		if(atIndex == -1) {
+			return false;
+		}
+		
+		Integer dotIndex = email.indexOf('.', atIndex);
+		Integer domainIndex = email.indexOf("gmail", atIndex);
+		
+		if(dotIndex == -1) {
+			return false;
+		}
+		
+		if(dotIndex == 0 || atIndex == email.length() - 1) {
+			return false;
+		}
+		
+		if(domainIndex > dotIndex) {
+			return false;
+		}
+		
+		
+		return true;
 	}
 	
 	public void validatePassword(String password) {
