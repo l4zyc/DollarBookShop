bdad21fc03be0b30e98246cf42774a0fb8bd2ac7
l4zyc <137969058+l4zyc@users.noreply.github.com>
2024-10-27 16:42:22 +0700

    Commit Init

--- DollarBookShop/src/util/Data.java
+++ DollarBookShop/src/util/Data.java
@@ -1,9 +1,29 @@
 package util;
 
-public class Data {
+import java.sql.SQLException;
 
-	public Data() {
+import model.User;
+
+public class Data {
+	
+	private Connect connect = Connect.getInstance();
+	
+	public User getUserData(String email, String password) throws NullPointerException { 
 		
+		String query = "SELECT * FROM MsUser";
+		
+		connect.rs = connect.execQuery(query);
+		User user = null;
+		
+		try {
+			while(connect.rs.next()) {
+				
+			}
+		} catch (Exception e) {
+			System.out.println("User Doesnt exist");
+		}
+		
+		return null;
 	}
 
 }
