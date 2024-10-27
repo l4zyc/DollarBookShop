114cb9b5f8180e3e3696955af817da0d80e8af31
l4zyC <appleyoukick@gmail.com>
2024-10-27 20:28:08 +0700

    Commit Init

--- DollarBookShop/src/view/HomeView.java
+++ DollarBookShop/src/view/HomeView.java
@@ -1,5 +1,8 @@
 package view;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.PauseTransition;
+import javafx.animation.Timeline;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
@@ -9,57 +12,84 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import model.User;
 
-public class HomeView extends HomeViewTemplate{
+public class HomeView extends HomeViewTemplate {
 
-	private Scene scene;
-	private BorderPane mainLayout;
-	private User user;
-	
-	private Label welcomeLbl;
-	private MenuBar mb; 
-	private Menu actionMenu;
-	private MenuItem home, cart, logout;
-	
-	public HomeView(Stage stage, User user) {
-		super(stage, user);
-		this.user = user;
-		
-		start();
-		arrangeComponent();
-	}
+    private Scene scene;
+    private BorderPane mainLayout;
+    private User user;
+    
+    private Label welcomeLbl;
+    private MenuBar mb; 
+    private Menu actionMenu;
+    private MenuItem home, cart, logout;
+    
+    public HomeView(Stage stage, User user) {
+        super(stage, user);
+        this.user = user;
+        
+        start();
+        arrangeComponent();
+        startTypingAnimation();
+    }
 
-	@Override
-	public void start() {
-		mainLayout = new BorderPane();
-		scene = new Scene(mainLayout, width, height);
-		
-		welcomeLbl = new Label(String.format("Welcome, %s", user.getUsername()));
-		mb = new MenuBar();
-		actionMenu = new Menu("Action");
-		home = new MenuItem("Home");
-		cart = new MenuItem("Cart");
-		logout = new MenuItem("Log Out");
-	}
+    @Override
+    public void start() {
+        mainLayout = new BorderPane();
+        scene = new Scene(mainLayout, width, height);
+        
+        welcomeLbl = new Label();
+        welcomeLbl.setFont(Font.font("Arial", FontWeight.BOLD, 45));
+        
+        mb = new MenuBar();
+        actionMenu = new Menu("Action");
+        home = new MenuItem("Home");
+        cart = new MenuItem("Cart");
+        logout = new MenuItem("Log Out");
+        
+        home.setDisable(true);
+    }
 
-	@Override
-	public void arrangeComponent() {
-		// TODO Auto-generated method stub
-		
-		welcomeLbl.setFont(Font.font("Arial", FontWeight.BOLD, 45));
-		
-		mb.getMenus().add(actionMenu);
-		actionMenu.getItems().add(home);
-		actionMenu.getItems().add(cart);
-		actionMenu.getItems().add(logout);
-		
-		mainLayout.setTop(mb);
-		mainLayout.setCenter(welcomeLbl);
-		
-		this.getStage().setScene(scene);
-	}
+    @Override
+    public void arrangeComponent() {
+        mb.getMenus().add(actionMenu);
+        actionMenu.getItems().addAll(home, cart, logout);
+        
+        mainLayout.setTop(mb);
+        mainLayout.setCenter(welcomeLbl);
+        
+        this.getStage().setScene(scene);
+    }
+    
+    private void startTypingAnimation() {
+        String baseText = "Welcome, ";
+        String username = user.getUsername();
+        Timeline timeline = new Timeline();
+        
+        
 
-	
+        for (int i = 0; i <= username.length(); i++) {
+            int index = i;
+            KeyFrame keyFrame = new KeyFrame(Duration.millis(200 * i),
+                    e -> welcomeLbl.setText(baseText + username.substring(0, index)));
+            timeline.getKeyFrames().add(keyFrame);
+        }
+        
+        KeyFrame delayFrame = new KeyFrame(Duration.millis(200 * username.length() + 5000),
+                e -> {});
+        timeline.getKeyFrames().add(delayFrame);
+        
+        for (int i = username.length(); i >= 0; i--) {
+            int index = i;
+            KeyFrame keyFrame = new KeyFrame(Duration.millis(200 * username.length() + 5000 + 200 * (username.length() - i)), 
+                    e -> welcomeLbl.setText(baseText + username.substring(0, index)));
+            timeline.getKeyFrames().add(keyFrame);
+        }
+       
 
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
 }
